79b88a2ebae63ab0e494f47066ba5b7fb451dd00
maasymaas16 <maasymaas16@gmail.com>
2022-07-11 21:44:08 -0600

    Added buttons for 0-9 and equals

--- myscript.js
+++ myscript.js
@@ -4,13 +4,34 @@ let totalNums = 9;
 
 const container = document.querySelector('.container');
 
+// const buttonOne = document.createElement('button');
+// buttonOne.classList.add('box');
+// buttonOne.textContent = '1';
+// container.appendChild(buttonOne);
+
 for (let i = 0; i < numRows; i++){
     const row = document.createElement('div');
     row.classList.add('row')
-    grid.appendChild(row);
+    container.appendChild(row)
     for (let j = 0; j < numCols; j++){
         const btn = document.createElement('button');
-        div.classList.add('box');
-        row.appendChild(div);
+        btn.classList.add('box');
+        btn.textContent = totalNums-2+j;
+        row.appendChild(btn);
     }
+    totalNums = totalNums - 3;
 }
+const bottomRow = document.createElement('div');
+bottomRow.classList.add('bottom');
+
+const zero = document.createElement('button');
+zero.classList.add('zero');
+zero.textContent = '0';
+
+const equal = document.createElement('button');
+equal.classList.add('box');
+equal.textContent = '=';
+
+bottomRow.appendChild(zero);
+bottomRow.appendChild(equal);
+container.appendChild(bottomRow);
