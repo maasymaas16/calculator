38c2d19a2ba1c9ed7e2b4fa676b42e5c5887a587
maasymaas16 <maasymaas16@gmail.com>
2022-07-20 20:49:40 -0600

    Added proper functions for first two numbers and first operator that display correctly on calculator UI

--- myscript.js
+++ myscript.js
@@ -1,5 +1,5 @@
-let num1 = 0;
-let num2 = 0;
+let num1 = '';
+let num2 = '';
 let operator = '';
 let curr = '';
 
@@ -24,20 +24,37 @@ const display = document.querySelector('.display');
 const number = document.querySelectorAll('.number');
 number.forEach((num) => {
     num.addEventListener('click', function () {
-        display.textContent += num.textContent;
+        if (operator == ''){
+            num1+=num.textContent;
+            display.textContent = num1;
+        } else {
+            num2 += num.textContent;
+            display.textContent = num2;
+        }
     });
 });
 
 const oper = document.querySelectorAll('.operator');
 oper.forEach((op) => {
     op.addEventListener('click', function () {
-        display.textContent = op.textContent;
+        if (operator == '') {
+        operator = op.textContent;
+        } else if (operator == '+'){
+            display.textContent = add(parseInt(num1), parseInt(num2));
+        } else if (operator == '-'){
+            display.textContent = subtract(parseInt(num1), parseInt(num2));
+        } else if (operator == '×'){
+            display.textContent = multiply(parseInt(num1), parseInt(num2));
+        } else {
+            display.textContent = div(parseInt(num1), parseInt(num2));
+        }  
     });
 });
-
 
 const clear = document.querySelector('.clear');
 clear.addEventListener('click', function () {
     display.textContent = '0';
+    num1 = '';
+    num2 = '';
 });
 
